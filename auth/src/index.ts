34e7fdbb03431163b47a307b5310498c41b8f627
Jovan <jovansr@protonmail.com>
2023-05-15 19:40:30 +0200

    k8s & skaffold setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,14 @@ import { json } from 'body-parser'
 const app = express()
 app.use(json())
 
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hi there')
+})
+
+app.get('/api/users/my-profile', (req, res) => {
+  res.send('My profile')
+})
+
 app.listen(3000, () => {
   console.log('Listening on 3000')
 })
