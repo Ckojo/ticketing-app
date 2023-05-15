bfae7bbcefe265a89da19fb0c83f8ac02648be11
Jovan <jovansr@protonmail.com>
2023-05-15 22:05:14 +0200

    DEV-3 DEV-3: Add routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,16 +1,17 @@
 import express from 'express'
 import { json } from 'body-parser'
 
+import { currentUserRouter } from './routes/current-user'
+import { signInRouter } from './routes/signin'
+import { signOutRouter } from './routes/signout'
+import { signUpRouter } from './routes/signup'
+
 const app = express()
 app.use(json())
-
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there')
-})
-
-app.get('/api/users/my-profile', (req, res) => {
-  res.send('My profile')
-})
+app.use(currentUserRouter)
+app.use(signInRouter)
+app.use(signOutRouter)
+app.use(signUpRouter)
 
 app.listen(3000, () => {
   console.log('Listening on 3000')
